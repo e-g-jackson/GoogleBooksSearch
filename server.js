25962b25a8566084848ab5b1ca3b64f8e466544e
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-02-24 03:55:55 -0600

    delete

--- server.js
+++ server.js
@@ -37,43 +37,19 @@ app.get('/api/books', (req, res) => {
 
 app.post('/api/books', (req, res) => {
     dbRoute.create(req.body).then(data =>{
-        console.log(data);
         res.send("Book Saved!")
     }).catch(err => {throw err;})
 })
 
 app.delete('/api/books/:id', (req, res) => {
-    //delete book
     const bookId = req.params.id
-    
-    // dbRoute.find({_id: bookId}, {justOne: true}).then((response)=>{
-    //     console.log("Response is:")
-    //     console.log(response)
-    // })
-    // dbRoute.find({_id: bookId}, {justOne: true}).remove((err, response) => {
-    //     console.log(response)
-    //     if(err){throw err}
-    //     else {res.send(JSON.stringify(response))};
-    // })
-    console.log('bookId = ', bookId)
-    // dbRoute.findByIdAndRemove({bookId})
-    //     .then((data)=>{
-    //         console.log('then')
-    //         console.log(data)
-    //         res.send(data)
-    //     }).catch((error) => {
-    //         console.log('catch')
-    //         console.log(error)
-    //         res.send(error)
-    //     });
-    dbRoute.remove({"id": bookId}, function(error, data){
+    dbRoute.deleteOne({"id": bookId}, function(error, data){
         if (error) throw error;
-        res.redirect('./saved')
+        res.send('Book Deleted!')
     })
 })
 
 app.use('*', (req, res) => {
-    // res.send()
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
